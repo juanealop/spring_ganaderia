54b6c61af56137659e61923972c8b37ddf9f7fe6
juanealop <88118954+juanealop@users.noreply.github.com>
2025-07-26 16:01:52 -0500

    commit inicial

---
+++ src/main/java/com/drsol/spring/springboot_administracion/SpringbootAdministracionApplication.java
@@ -0,0 +1,13 @@
+package com.drsol.spring.springboot_administracion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAdministracionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAdministracionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drsol/spring/springboot_administracion/SpringbootAdministracionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drsol.spring.springboot_administracion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAdministracionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
