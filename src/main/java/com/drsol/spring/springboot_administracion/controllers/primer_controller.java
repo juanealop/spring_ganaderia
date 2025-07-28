947fe3514e59eb9ae67cc0e62eadb9709bf0ba1d
juanealop <88118954+juanealop@users.noreply.github.com>
2025-07-28 17:03:55 -0500

    por primera vez se pasaron datos a la vista desde el controller

--- src/main/java/com/drsol/spring/springboot_administracion/controllers/primer_controller.java
+++ src/main/java/com/drsol/spring/springboot_administracion/controllers/primer_controller.java
@@ -2,20 +2,28 @@ package com.drsol.spring.springboot_administracion.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestParam; 
+import org.springframework.ui.Model;
 
 
 @Controller
 public class primer_controller {
 
      @GetMapping("/pantalla_ejemplo")
-     
-     
-
+     /* 
     public String info(){
 
+        esto retorna la ruta de la pagina     
 
         return "pantalla_ejemplo";
+    }   
+
+    */ 
+    public String info(Model model){
+        model.addAttribute("titulo", "servidor en linea");
+        model.addAttribute("Servidor", "informacion_config");
+        return "pantalla_ejemplo";   //Pasar datos a la vista
     }
+
 
 }
